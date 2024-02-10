40bed3a1d09b5149fdf25623b41082269de51982
udaynaliyapara <uday.workin@gmail.com>
2024-02-11 01:12:29 +0530

    register controller

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen(process.env.PORT || 9000, () => {
       console.log(`Server is running at port : ${process.env.PORT}`);
     });
   })
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -10,20 +10,18 @@ cloudinary.config({
 const uploadOnCloudinary = async (localFilePath) => {
   try {
     if (!localFilePath) return null;
+    //upload the file on cloudinary
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
-    console.log("File is uploaded on cloudnary", response.url);
+    // file has been uploaded successfull
+    console.log("file is uploaded on cloudinary ", response.url);
     return response;
   } catch (error) {
-    fs.unlinkSync(localFilePath); // remove the locally saved file if failed
+    console.log("error", error);
+    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
+    return null;
   }
 };
 
-cloudinary.uploader.upload(
-  "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-  { public_id: "olympic_flag" },
-  function (error, result) {
-    console.log(result);
-  },
-);
+export { uploadOnCloudinary };
